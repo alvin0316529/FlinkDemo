98362dc9633cfd6fa88198c065d3da4ba82153fd
alvin0316529 <2271700484@qq.com>
2019-11-09 13:59:56 +0800

    first commit

---
+++ src/main/scala/Flink/WordCount.scala
@@ -0,0 +1,26 @@
+package Flink
+
+import org.apache.flink.api.scala._
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    //创建一个批处理的执行环境
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    val inputPath = "F:\\data\\spark\\spark1.txt"
+
+    //从文件中读取数据
+    val inputDataSet = env.readTextFile(inputPath)
+
+    //对dataset进行word count 处理
+    val wordCountDataSet = inputDataSet.flatMap(_.split(" "))
+      .map(word => (word,1))
+      .groupBy(0)
+      .sum(1)
+
+
+    //输出
+    wordCountDataSet.print()
+
+  }
+}
